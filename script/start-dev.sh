84b72ea4344e5e651df2e3ea3d9fc49b3ce4f20b
Kent slaymaker <kmwalke@gmail.com>
2020-06-10 15:23:19 -0600

    Setup DB

--- script/start-dev.sh
+++ script/start-dev.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 echo 'system started'
 
-#psql -h $DB_HOST -d $DB_DEV -U $DB_USERNAME -c 'SELECT id FROM users LIMIT 1'
+psql -h $DB_HOST -d $DB_DEV -U $DB_USERNAME -c 'SELECT id FROM users LIMIT 1'
 
-#if [ $? == 0 ]; then
-#  echo "DB already set up"
-#else
-#  echo "Setting up the DB"
-#  bundle exec rake db:create
-#  bundle exec rake db:schema:load
-#fi
+if [ $? == 0 ]; then
+  echo "DB already set up"
+else
+  echo "Setting up the DB"
+  bundle exec rake db:create
+  bundle exec rake db:schema:load
+fi
+
+bundle exec rails db:migrate
 
 bundle exec puma -p 3000 -C config/puma.rb
