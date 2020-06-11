79fb3f97025e84004663d21f4143a98804ff6e02
Kent Slaymaker <kent.slaymaker@travelershaven.com>
2020-06-11 08:10:01 -0600

    homepage specs

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ end
 
 group :test do
   gem 'capybara', '>= 2.15'
+  gem 'rails-controller-testing'
   gem 'selenium-webdriver'
   gem 'webdrivers'
 end
--- spec/requests/home_request_spec.rb
+++ spec/requests/home_request_spec.rb
@@ -1,4 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "Home", type: :request do
+  it "renders the homepage" do
+    get root_path
+    expect(response).to render_template(:index)
+  end
+
+  it "has the proper content on homepage" do
+    get root_path
+    expect(response.body).to include('Kenobi')
+  end
 end
