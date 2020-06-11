f8144dc49b955b8bcf27696e34b09d7434f24c31
Kent Slaymaker <kent.slaymaker@travelershaven.com>
2020-06-11 11:44:17 -0600

    add sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-  resources :users
-  get 'home/index'
-
   root to: 'home#index'
+
+  resources :users
+
+  resources :sessions, only: [:new, :create, :destroy]
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 end
