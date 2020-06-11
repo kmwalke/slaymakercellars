29fbf577c30eb4e39e035914d388245de22ff1f8
Kent Slaymaker <kent.slaymaker@travelershaven.com>
2020-06-11 09:05:11 -0600

    formatting

--- spec/views/users/index.html.erb_spec.rb
+++ spec/views/users/index.html.erb_spec.rb
@@ -1,25 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe "users/index", type: :view do
+RSpec.describe 'users/index', type: :view do
   before(:each) do
     assign(:users, [
-      User.create!(
-        email: "Email",
-        password: "",
-        name: "Name"
-      ),
-      User.create!(
-        email: "Email",
-        password: "",
-        name: "Name"
-      )
-    ])
+             User.create!(
+               email: 'Email',
+               password: '',
+               name: 'Name'
+             ),
+             User.create!(
+               email: 'Email',
+               password: '',
+               name: 'Name'
+             )
+           ])
   end
 
-  it "renders a list of users" do
+  it 'renders a list of users' do
     render
-    assert_select "tr>td", text: "Email".to_s, count: 2
-    assert_select "tr>td", text: "".to_s, count: 2
-    assert_select "tr>td", text: "Name".to_s, count: 2
+    assert_select 'tr>td', text: 'Email'.to_s, count: 2
+    assert_select 'tr>td', text: ''.to_s, count: 2
+    assert_select 'tr>td', text: 'Name'.to_s, count: 2
   end
 end
