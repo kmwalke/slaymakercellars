244e763d354412f2bea29ec930d357b171e108bc
Kent Slaymaker <kent.slaymaker@travelershaven.com>
2020-06-12 08:20:42 -0600

    redirect to requested page when logging in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,13 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to root_url, notice: 'Logged in!'
+      if session[:orig_destination]
+        orig                       = session[:orig_destination]
+        session[:orig_destination] = nil
+        redirect_to orig, notice: 'Logged in!'
+      else
+        redirect_to root_url, notice: 'Logged in!'
+      end
     else
       flash.now[:alert] = 'Email or password is invalid'
       render 'new'
