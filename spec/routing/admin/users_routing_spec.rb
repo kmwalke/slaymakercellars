242e4b85ee83679365e71e691f4b671595500b37
Kent slaymaker <kmwalke@gmail.com>
2020-06-14 09:30:36 -0600

    Update user specs to include admin

---
+++ spec/routing/admin/users_routing_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe Admin::UsersController, type: :routing do
+  it 'routes to #index' do
+    expect(get: '/admin/users').to route_to('users#index')
+  end
+
+  it 'routes to #new' do
+    expect(get: '/admin/users/new').to route_to('users#new')
+  end
+
+  it 'routes to #edit' do
+    expect(get: '/admin/users/1/edit').to route_to('users#edit', id: '1')
+  end
+
+  it 'routes to #create' do
+    expect(post: '/admin/users').to route_to('users#create')
+  end
+
+  it 'routes to #update via PUT' do
+    expect(put: '/admin/users/1').to route_to('users#update', id: '1')
+  end
+
+  it 'routes to #update via PATCH' do
+    expect(patch: '/admin/users/1').to route_to('users#update', id: '1')
+  end
+
+  it 'routes to #destroy' do
+    expect(delete: '/admin/users/1').to route_to('users#destroy', id: '1')
+  end
+end
