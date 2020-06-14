4502dd1807b092957ea0fb363890c77d9a3494a2
Kent slaymaker <kmwalke@gmail.com>
2020-06-14 09:51:50 -0600

    Formatting

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,57 +1,55 @@
-class Admin::UsersController < ApplicationController
-  before_action :set_user, only: [:edit, :update, :destroy]
-  before_action :logged_in?
+module Admin
+  class UsersController < ApplicationController
+    before_action :set_user, only: [:edit, :update, :destroy]
+    before_action :logged_in?
 
-  def index
-    @users = User.all
-  end
+    def index
+      @users = User.all
+    end
 
-  def new
-    @user = User.new
-  end
+    def new
+      @user = User.new
+    end
 
-  def edit; end
+    def edit; end
 
-  def create
-    @user = User.new(user_params)
+    def create
+      @user = User.new(user_params)
 
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to admin_users_path, notice: 'User was successfully created.' }
-      else
-        format.html { render :new }
+      respond_to do |format|
+        if @user.save
+          format.html { redirect_to admin_users_path, notice: 'User was successfully created.' }
+        else
+          format.html { render :new }
+        end
       end
     end
-  end
 
-  # PATCH/PUT /users/1
-  def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to admin_users_path, notice: 'User was successfully updated.' }
-      else
-        format.html { render :edit }
+    def update
+      respond_to do |format|
+        if @user.update(user_params)
+          format.html { redirect_to admin_users_path, notice: 'User was successfully updated.' }
+        else
+          format.html { render :edit }
+        end
       end
     end
-  end
 
-  # DELETE /users/1
-  def destroy
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to admin_users_path, notice: 'User was successfully destroyed.' }
+    def destroy
+      @user.destroy
+      respond_to do |format|
+        format.html { redirect_to admin_users_path, notice: 'User was successfully destroyed.' }
+      end
     end
-  end
 
-  private
+    private
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_user
-    @user = User.find(params[:id])
-  end
+    def set_user
+      @user = User.find(params[:id])
+    end
 
-  # Only allow a list of trusted parameters through.
-  def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :name)
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation, :name)
+    end
   end
 end
