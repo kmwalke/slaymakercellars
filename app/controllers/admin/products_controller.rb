7e2ff9cc0510e86e2ebc6ff9bc59e489222450e8
Kent slaymaker <kmwalke@gmail.com>
2020-06-14 22:39:01 -0600

    Add public product routes

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -44,12 +44,10 @@ module Admin
 
     private
 
-    # Use callbacks to share common setup or constraints between actions.
     def set_product
       @product = Product.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def product_params
       params.require(:product).permit(:name, :price_point, :description)
     end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
 
+  get 'products', to: 'products#index'
+
   namespace :admin do
     get '/', to: 'home#index'
     resources :products
