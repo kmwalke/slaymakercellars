9df96d939bec24a6825501f7a047ee412bc2499c
Kent Slaymaker <kent.slaymaker@travelershaven.com>
2020-06-20 14:02:58 -0600

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_20_191130) do
+ActiveRecord::Schema.define(version: 2020_06_20_192613) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,8 +26,11 @@ ActiveRecord::Schema.define(version: 2020_06_20_191130) do
   create_table "states", force: :cascade do |t|
     t.string "name"
     t.string "abbreviation"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "towns", force: :cascade do |t|
+    t.string "name"
+    t.integer "state_id"
   end
 
   create_table "users", force: :cascade do |t|
