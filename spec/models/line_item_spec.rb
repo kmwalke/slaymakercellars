b83269f130703767011a6e2f4f3aaf111da62bec
Kent Slaymaker <kent@1bg.com>
2020-06-24 15:31:05 -0600

    add line_item spec

--- spec/models/line_item_spec.rb
+++ spec/models/line_item_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe LineItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should require a quantity' do
+    expect(LineItem.create(quantity: '').errors).to have_key(:quantity)
+  end
+
+  it 'should require a price' do
+    expect(LineItem.create(price: '').errors).to have_key(:price)
+  end
+
+  it 'should require a product' do
+    expect(LineItem.create(product_id: '').errors).to have_key(:product_id)
+  end
+
+  it 'should require a PO' do
+    expect(LineItem.create(purchase_order_id: '').errors).to have_key(:purchase_order_id)
+  end
 end
