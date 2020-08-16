82f02a4b98dc42990ebf1c9a92feca1e065cc7d0
Kent Slaymaker <kent@1bg.com>
2020-08-15 19:11:39 -0600

    add contacts page

--- spec/features/contacts_spec.rb
+++ spec/features/contacts_spec.rb
@@ -5,12 +5,9 @@ RSpec.feature 'Contacts', type: :feature do
   let!(:contact) { Contact.create(name: 'john', town: town) }
   let!(:deleted_contact) { Contact.create(name: 'deleted contact', town: town, deleted_at: DateTime.now) }
 
-  before :each do
-    login
-  end
-
   scenario 'list contacts' do
-    visit contacts_path
+    visit root_path
+    click_link 'Where to Buy'
     expect(page).to have_content(contact.name)
     expect(page).not_to have_content(deleted_contact.name)
   end
--- spec/routing/contacts_routing_spec.rb
+++ spec/routing/contacts_routing_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.describe ContactsController, type: :routing do
   it 'routes to #index' do
-    expect(get: '/contacts').to route_to('contact#index')
+    expect(get: '/contacts').to route_to('contacts#index')
   end
 end
