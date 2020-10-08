b53596fdc36a2d477b5863a5071b3bc1049ba98a
Kent slaymaker <kmwalke@gmail.com>
2020-10-08 09:48:30 -0600

    Change header link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :logged_in?
 
   def wine_list
-    @wine_list || Vinochipper.wine_list(3005)
+    @wine_list || VinoChipper.wine_list(3005)
   end
 
   def current_user
