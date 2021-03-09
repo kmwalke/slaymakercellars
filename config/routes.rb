9430b32a2a0a4906985c4747faadf1e9559e8141
Kent Slaymaker <kent@1bg.com>
2021-03-09 15:43:25 -0700

    handle 404s

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   root to: 'home#index'
-  if Rails.env.production?
-    get '404', :to => 'home#index'
-  end
+  get '404', :to => 'home#index'
 
   resources :sessions, only: [:new, :create, :destroy]
   get 'login', to: 'sessions#new', as: 'login'
