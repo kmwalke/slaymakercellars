c952bda8b1a8ffdb7f6b0aa34015d6dee1c4cf9a
Kent Slaymaker <kent@1bg.com>
2021-05-11 12:00:56 -0600

    add more contacts to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,19 +12,17 @@ User.create(email: 'cris@slaymakercellars.com', name: 'cris', password: '123')
 Product.create(name: 'Traditional', price_point: 10, description: 'This is the description')
 Product.create(name: 'Sweet Clover', price_point: 12, description: 'This is the description')
 
-ar = State.create(name: 'Arkansas', abbreviation: 'AR')
 co = State.create(name: 'Colorado', abbreviation: 'CO')
 
-idaho_springs = Town.create(name: 'Idaho Springs', state: co)
-georgetown    = Town.create(name: 'Georgetown', state: co)
-little_rock   = Town.create(name: 'Little Rock', state: ar)
+20.times do |i|
+  Town.create(name: "Town #{i}", state: co)
+end
 
-Contact.create(name: 'A bar', town: idaho_springs, url: 'http://www.thing.com')
-Contact.create(name: 'A package store', town: idaho_springs, url: 'http://www.thing.com')
-Contact.create(name: 'A restaurant', town: idaho_springs)
-Contact.create(name: 'B bar', town: georgetown, url: 'http://www.thing.com')
-Contact.create(name: 'B package store', town: georgetown, url: 'http://www.thing.com')
-Contact.create(name: 'B restaurant', town: georgetown)
-Contact.create(name: 'C bar', town: little_rock, url: 'http://www.thing.com')
-Contact.create(name: 'C package store', town: little_rock, url: 'http://www.thing.com')
-Contact.create(name: 'C restaurant', town: little_rock, url: 'http://www.thing.com')
+
+Town.all.each do |town|
+  (1 + rand(6)).times do |i|
+    contact_url = [true,false].sample == true ? 'http://www.thing.com' : nil
+    Contact.create(name: "Contact_#{town.id}_#{i}", town: town, url: contact_url)
+  end
+end
+
