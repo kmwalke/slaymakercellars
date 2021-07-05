c286d3d582e998fc38a33415a6ad6442d0c8bd16
Kent Slaymaker <kentslaymaker@gmail.com>
2021-07-05 09:40:20 -0600

    [dashboard] added dashboard

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -2,6 +2,11 @@ module Admin
   class HomeController < ApplicationController
     before_action :logged_in?
 
-    def index; end
+    def index
+      @contacts        = Contact.count
+      @urgent_contacts = Contact.urgent.count
+      @states          = State.count
+      @towns           = Town.count
+    end
   end
 end
