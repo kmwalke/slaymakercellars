531cc9770a25f704e81af40492e543b543eb96af
Kent Slaymaker <kentslaymaker@gmail.com>
2021-07-08 15:25:27 -0600

    add visit page

--- spec/features/visit_spec.rb
+++ spec/features/visit_spec.rb
@@ -4,6 +4,6 @@ RSpec.feature 'Visit', type: :feature do
   scenario 'displays the visit page' do
     visit visit_path
 
-    expect(page).to have_content('pop-up tasting room')
+    expect(page).to have_content('tasting room')
   end
 end
