5c8e3449014fd6dd10f3c6ef56e1dc8042c963e3
Kent Slaymaker <kentslaymaker@gmail.com>
2021-10-03 12:22:39 -0600

    [products] add category to admin/product pages

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,9 +2,9 @@ class Product < ApplicationRecord
   validates :name, presence: true
   validates :price_point, presence: true
 
-  CATEGORIES = {
-    flagship: 'Flagship',
-    mazer: 'Mazer Series',
-    dessert: 'Dessert'
-  }.freeze
+  CATEGORIES = [
+    'Flagship',
+    'Mazer Series',
+    'Dessert'
+  ].freeze
 end
--- db/migrate/20211003165441_add_type_to_products.rb
+++ db/migrate/20211003165441_add_type_to_products.rb
@@ -1,5 +1,5 @@
 class AddTypeToProducts < ActiveRecord::Migration[6.1]
   def change
-    add_column :products, :category, :string, null: false, default: Product::CATEGORIES[:flagship]
+    add_column :products, :category, :string, null: false, default: Product::CATEGORIES.first
   end
 end
