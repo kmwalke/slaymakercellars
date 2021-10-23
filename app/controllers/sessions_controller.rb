202b932b81fa1e06c41cd8f44714286938767208
Kent Slaymaker <kentslaymaker@gmail.com>
2021-10-23 12:24:56 -0600

    [xero] formatting

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,8 @@ class SessionsController < ApplicationController
   def create
     auth_hash = request.env['omniauth.auth']
 
-    user                    = User.find_or_create_by(xeroUid: auth_hash['uid'], name: auth_hash['info']['name'],
+    user                    = User.find_or_create_by(xeroUid: auth_hash['uid'],
+                                                     name: auth_hash['info']['name'],
                                                      email: auth_hash['info']['email'])
     user.xeroAccessToken    = auth_hash['credentials']['token']
     user.xeroRefreshToken   = auth_hash['credentials']['refresh_token']
