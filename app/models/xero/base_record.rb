ed74d2852c9ddb80bc60d043b6d8b318df928e52
Kent Slaymaker <kentslaymaker@gmail.com>
2021-10-24 18:55:28 -0600

    move common code to base xero object

--- app/models/xero/base_record.rb
+++ app/models/xero/base_record.rb
@@ -3,10 +3,28 @@ module Xero
     XERO_TOKEN_ENDPOINT = 'https://identity.xero.com/connect/token'.freeze
     XERO_API_URL        = 'https://api.xero.com/api.xro/2.0/'.freeze
 
-    def initialize(response); end
+    attr_reader :id, :errors
+
+    def initialize(response)
+      body = JSON.parse(response.body)
+      if response.status == 400
+        @errors = body['Elements'][0]['ValidationErrors']
+        return
+      end
+
+      @id = body['Contacts'][0]['ContactID']
+    end
 
     def self.create
       raise NotImplementedError
+    end
+
+    def self.save_xero_errors(object, xero_object)
+      xero_object.errors&.each do |error|
+        object.xero_sync_errors << XeroSyncError.new(message: error['Message'])
+      end
+
+      xero_object
     end
 
     def self.xero_api_get(user, endpoint)
--- app/models/xero/contact.rb
+++ app/models/xero/contact.rb
@@ -2,17 +2,8 @@ module Xero
   class Contact < Xero::BaseRecord
     ENDPOINT = 'Contacts'.freeze
 
-    attr_reader :id, :errors
-
     def initialize(response)
       super
-      body = JSON.parse(response.body)
-      if response.status == 400
-        @errors = body['Elements'][0]['ValidationErrors']
-        return
-      end
-
-      @id = body['Contacts'][0]['ContactID']
     end
 
     def self.create(user, contact)
@@ -20,14 +11,6 @@ module Xero
 
       contact.xero_sync_errors.each(&:destroy)
       save_xero_errors(contact, Xero::Contact.new(xero_api_post(user, ENDPOINT, body_params(contact))))
-    end
-
-    def self.save_xero_errors(contact, xero_contact)
-      xero_contact.errors&.each do |error|
-        contact.xero_sync_errors << XeroSyncError.new(message: error['Message'])
-      end
-
-      xero_contact
     end
 
     def self.body_params(contact)
