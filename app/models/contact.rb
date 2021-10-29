e1a0cf18bdb670745caeef07882285ef4e5f1efb
Kent Slaymaker <kentslaymaker@gmail.com>
2021-10-29 08:27:41 -0600

    [invoices] revert has_many change

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -6,6 +6,7 @@ class Contact < ApplicationRecord
   belongs_to :town
   has_many :notes
   has_many :orders
+  has_many :xero_sync_errors, as: :syncable
 
   validates :name, presence: true, uniqueness: true
   validates :town_id, presence: true
--- app/models/xero/syncable.rb
+++ app/models/xero/syncable.rb
@@ -2,8 +2,6 @@ module Xero
   module Syncable
     extend ActiveSupport::Concern
 
-    has_many :xero_sync_errors, as: :syncable
-
     def synced?
       xero_id && xero_sync_errors.empty?
     end
