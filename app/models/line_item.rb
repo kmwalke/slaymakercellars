9b5b5505937aa1a1be56e30cc7d122d466fb4156
Kent Slaymaker <kentslaymaker@gmail.com>
2021-11-07 12:45:11 -0700

    [140_customer_pricing] add contact case pricing logic to line item pricing

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -5,14 +5,18 @@ class LineItem < ApplicationRecord
   validates :quantity, presence: true
 
   def price_point
-    return product.price_point if quantity < product.case_size
+    return case_price if gets_case_price
 
-    case_price
+    product.price_point
   end
 
   private
 
   def case_price
     (product.price_point * (product.case_size - 1)) / product.case_size
+  end
+
+  def gets_case_price
+    quantity >= product.case_size || order.contact.always_gets_case_deal
   end
 end
