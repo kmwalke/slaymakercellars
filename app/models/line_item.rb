af0e1a607cec7d9969b18e1faf557283093f1873
Kent Slaymaker <kentslaymaker@gmail.com>
2021-11-07 11:44:45 -0700

    [140_customer_pricing] move case pricing logic to line_items

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -3,4 +3,14 @@ class LineItem < ApplicationRecord
   belongs_to :product
 
   validates :quantity, presence: true
+
+  def price_point
+    return product.price_point if quantity < product.case_size
+
+    case_price
+  end
+
+  def case_price
+    (product.price_point * (product.case_size - 1)) / product.case_size
+  end
 end
