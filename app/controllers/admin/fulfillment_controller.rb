f863e00e305e185a5ac155830190e35bd1842f92
Kent Slaymaker <kentslaymaker@gmail.com>
2021-12-11 15:07:57 -0700

    [fulfillment] add fulfillment page

--- app/controllers/admin/fulfillment_controller.rb
+++ app/controllers/admin/fulfillment_controller.rb
@@ -1,33 +1,11 @@
 module Admin
   class FulfillmentController < ApplicationController
+    include OrderFulfillment
+
     before_action :logged_in?
 
     def index
       @fulfillment = order_fulfillment
-    end
-
-    private
-
-    def order_fulfillment
-      result = {}
-      week   = Date.today...Date.today + 7
-
-      week.each do |day|
-        result[day] = {}
-
-        Product.all.each do |product|
-          result[day][product.name] = {}
-          total                     = 0
-
-          Order.to_be_fulfilled(day).each do |order|
-            li                  = order.line_items.where(product_id: product.id)
-            li.map { |l| total += l.quantity }
-          end
-          result[day][product.name] = total
-        end
-      end
-
-      result
     end
   end
 end
