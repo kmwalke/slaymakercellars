6333402078a3a68642522beb92a67e4df507ec66
Kent Slaymaker <kentslaymaker@gmail.com>
2021-12-11 15:51:27 -0700

    [fulfillment] use fulfillmentPlan class

--- app/controllers/admin/fulfillment_controller.rb
+++ app/controllers/admin/fulfillment_controller.rb
@@ -1,11 +1,43 @@
 module Admin
   class FulfillmentController < ApplicationController
-    include OrderFulfillment
-
     before_action :logged_in?
 
     def index
-      @fulfillment = order_fulfillment
+      @fulfillment = FulfillmentPlan.new
+    end
+  end
+end
+
+class FulfillmentPlan
+  attr_reader :days
+
+  def initialize
+    @plan = {}
+    @days = Date.today...Date.today + 7
+    fill_plan
+
+    super
+  end
+
+  def day_plan(date)
+    @plan[date]
+  end
+
+  private
+
+  def fill_plan
+    days.each do |day|
+      @plan[day] = {}
+
+      Product.all.each do |product|
+        @plan[day][product.name.to_s] = 0
+
+        Order.to_be_fulfilled(day).each do |order|
+          order.line_items.where(product_id: product.id).map do |line_item|
+            @plan[day][product.name.to_s] += line_item.quantity
+          end
+        end
+      end
     end
   end
 end
