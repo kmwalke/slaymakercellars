4dce448ef9b557f240ca617d030848f0442e7d23
Kent Slaymaker <kentslaymaker@gmail.com>
2021-12-18 15:25:34 -0700

    [testing] upgrade product_spec

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,28 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  let!(:product) do
-    Product.create(name: 'product_1', price_point: 14, description: 'This is the description',
-                   category: Product::CATEGORIES.last, xero_code: 'duct_1')
-  end
-
   it 'should require a name' do
     expect(Product.create(name: '').errors).to have_key(:name)
   end
 
   it 'should require a unique name' do
-    expect(Product.create(name: 'product_1').errors).to have_key(:name)
+    product = FactoryBot.create(:product)
+    expect { FactoryBot.create(:product, name: product.name) }.to raise_error(ActiveRecord::RecordInvalid)
   end
 
   it 'should require a xero id' do
-    expect(Product.create(xero_code: '').errors).to have_key(:xero_code)
+    expect { FactoryBot.create(:product, xero_code: nil) }.to raise_error(ActiveRecord::RecordInvalid)
   end
 
   it 'should require a unique xero id' do
-    expect(Product.create(xero_code: 'duct_1').errors).to have_key(:xero_code)
+    product = FactoryBot.create(:product)
+    expect { FactoryBot.create(:product, xero_code: product.xero_code) }.to raise_error(ActiveRecord::RecordInvalid)
   end
 
   it 'should require a price point' do
-    expect(Product.create(price_point: '').errors).to have_key(:price_point)
+    expect { FactoryBot.create(:product, price_point: nil) }.to raise_error(ActiveRecord::RecordInvalid)
   end
 end
