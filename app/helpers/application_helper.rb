0b948368be9d32ac2016748fca937ffcaa867ec1
Kent Slaymaker <kentslaymaker@gmail.com>
2022-01-08 12:02:34 -0700

    [list-usability] move sort logic to application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,22 @@ module ApplicationHelper
   def humanize_date_time(date_time)
     date_time.strftime('%A, %B %d, %Y - %I:%M %p')
   end
+
+  def sortable(name)
+    link_to name, { sort: name.downcase, direction: sort_direction(name) }, { class: link_class(name) }
+  end
+
+  private
+
+  def sort_direction(name)
+    return 'asc' unless name.downcase == params[:sort]
+
+    params[:direction] == 'asc' ? 'desc' : 'asc'
+  end
+
+  def link_class(name)
+    return '' unless name.downcase == params[:sort]
+
+    params[:direction] == 'asc' ? 'down' : 'up'
+  end
 end
--- app/helpers/contacts_helper.rb
+++ app/helpers/contacts_helper.rb
@@ -1,19 +1,2 @@
 module ContactsHelper
-  def sortable(name)
-    link_to name, { sort: name.downcase, direction: sort_direction(name) }, { class: link_class(name) }
-  end
-
-  private
-
-  def sort_direction(name)
-    return 'asc' unless name.downcase == params[:sort]
-
-    params[:direction] == 'asc' ? 'desc' : 'asc'
-  end
-
-  def link_class(name)
-    return '' unless name.downcase == params[:sort]
-
-    params[:direction] == 'asc' ? 'down' : 'up'
-  end
 end
