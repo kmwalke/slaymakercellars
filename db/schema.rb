35dff38a115ee79d5aa6cd03e78ebf47ecaab00f
Kent Slaymaker <kentslaymaker@gmail.com>
2022-01-31 13:40:45 -0700

    [resolved_notes] add resolution field to notes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_26_183402) do
+ActiveRecord::Schema.define(version: 2022_01_31_203850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2022_01_26_183402) do
     t.datetime "resolved_at", precision: 6
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "resolution"
   end
 
   create_table "orders", force: :cascade do |t|
