fa75b7dbf5072e23ec530e7936b87970a981c6c7
Kent Slaymaker <kmwalke@gmail.com>
2022-02-25 15:20:41 -0700

    customer homepage spec init version

--- spec/features/customer/home_spec.rb
+++ spec/features/customer/home_spec.rb
@@ -16,37 +16,12 @@ RSpec.feature 'Customer::Home', type: :feature do
 
   describe 'logged in' do
     before :each do
-      login_as_admin
-      visit '/admin'
+      login_as_customer
+      visit '/customer'
     end
 
     scenario 'display page' do
-      expect(page).to have_content('Administration')
-    end
-
-    scenario 'user link' do
-      first(:link, 'Users').click
-      expect(current_path).to eq(admin_users_path)
-    end
-
-    scenario 'product link' do
-      first(:link, 'Products').click
-      expect(current_path).to eq(admin_products_path)
-    end
-
-    scenario 'contact link' do
-      first(:link, 'Contacts').click
-      expect(current_path).to eq(admin_contacts_path)
-    end
-
-    scenario 'towns link' do
-      first(:link, 'View Towns').click
-      expect(current_path).to eq(admin_towns_path)
-    end
-
-    scenario 'states link' do
-      first(:link, 'View States').click
-      expect(current_path).to eq(admin_states_path)
+      expect(page).to have_content('Customer Portal')
     end
   end
 end
