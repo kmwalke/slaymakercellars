1c2bd7c0dfc80f99bbf7c5ed5333e0f53d44c04c
Kent Slaymaker <kmwalke@gmail.com>
2022-03-02 12:25:09 -0700

    add receives_email to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_02_171338) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_02_192315) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -108,6 +108,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_02_171338) do
     t.string "xeroTokenExpiresAt"
     t.string "role", null: false
     t.integer "contact_id"
+    t.boolean "receives_emails", default: true, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
