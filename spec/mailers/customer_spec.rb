8909cbd624d7296f047e9aacf60bb79574b4f74b
Kent Slaymaker <kmwalke@gmail.com>
2022-03-03 14:01:40 -0700

    mailer specs

--- spec/mailers/customer_spec.rb
+++ spec/mailers/customer_spec.rb
@@ -4,20 +4,21 @@ RSpec.describe CustomerMailer, type: :mailer do
   let(:customer) { FactoryBot.create(:customer) }
 
   it 'sends welcome email' do
-    expect(
-      CustomerMailer.with(user: customer).welcome
-    ).to be_kind_of(ActionMailer::Parameterized::MessageDelivery)
+    expect do
+      CustomerMailer.with(user: customer).welcome.deliver_now
+    end.to change { ActionMailer::Base.deliveries.count }.by(1)
   end
 
   it 'sends new user email' do
-    expect(
-      CustomerMailer.with(user: customer).account_activated
-    ).to be_kind_of(ActionMailer::Parameterized::MessageDelivery)
+    FactoryBot.create(:admin)
+    expect do
+      CustomerMailer.with(user: customer).new_customer.deliver_now
+    end.to change { ActionMailer::Base.deliveries.count }.by(1)
   end
 
   it 'sends account activated email' do
-    expect(
-      CustomerMailer.with(user: customer).new_customer
-    ).to be_kind_of(ActionMailer::Parameterized::MessageDelivery)
+    expect do
+      CustomerMailer.with(user: customer).account_activated.deliver_now
+    end.to change { ActionMailer::Base.deliveries.count }.by(1)
   end
 end
