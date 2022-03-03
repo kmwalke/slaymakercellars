0a1d0f8eb4a1c56864e1903326ac0497b6ebe4d4
Kent Slaymaker <kmwalke@gmail.com>
2022-03-03 13:20:34 -0700

    add customer specs

--- spec/mailers/customer_spec.rb
+++ spec/mailers/customer_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe CustomerMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:customer) { FactoryBot.create(:customer) }
+
+  it 'sends welcome email' do
+    expect(
+      CustomerMailer.with(user: customer).welcome
+    ).to be_kind_of(ActionMailer::Parameterized::MessageDelivery)
+  end
+
+  it 'sends new user email' do
+    expect(
+      CustomerMailer.with(user: customer).account_activated
+    ).to be_kind_of(ActionMailer::Parameterized::MessageDelivery)
+  end
+
+  it 'sends account activated email' do
+    expect(
+      CustomerMailer.with(user: customer).new_customer
+    ).to be_kind_of(ActionMailer::Parameterized::MessageDelivery)
+  end
 end
--- spec/mailers/previews/customer_preview.rb
+++ spec/mailers/previews/customer_preview.rb
@@ -3,4 +3,12 @@ class CustomerPreview < ActionMailer::Preview
   def welcome
     CustomerMailer.with(user: FactoryBot.build(:customer)).welcome
   end
+
+  def account_activated
+    CustomerMailer.with(user: FactoryBot.build(:customer)).account_activated
+  end
+
+  def new_customer
+    CustomerMailer.with(user: FactoryBot.build(:customer)).new_customer
+  end
 end
