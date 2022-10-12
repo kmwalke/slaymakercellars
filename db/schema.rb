fa09a0602879b72c580c2e0dd65bc767c97f1106
Kent Slaymaker <kmwalke@gmail.com>
2022-10-12 13:50:00 -0600

    Add unit number to contacts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_01_001321) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_12_194614) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,6 +36,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_01_001321) do
     t.boolean "paperless_billing", default: false, null: false
     t.boolean "always_gets_case_deal", default: false, null: false
     t.boolean "is_public", default: true, null: false
+    t.string "unit_number"
   end
 
   create_table "line_items", force: :cascade do |t|
