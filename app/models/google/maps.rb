f99b0d8d8d5fac360a4e0b22f0de8739d36756d7
Kent Slaymaker <kmwalke@gmail.com>
2023-02-18 14:09:45 -0700

    update visit us page

--- app/models/google/maps.rb
+++ app/models/google/maps.rb
@@ -3,10 +3,14 @@
 
 module Google
   class Maps < UrlParams
-    GOOGLE_MAPS_BASE_URL = 'https://www.google.com/maps?q='.freeze
+    GOOGLE_MAPS_BASE_URL = 'https://www.google.com/maps'.freeze
 
     def self.query_url(address)
-      GOOGLE_MAPS_BASE_URL + sanitize(address)
+      GOOGLE_MAPS_BASE_URL + '?q=' + sanitize(address)
+    end
+
+    def self.embed_url(address)
+      "https://www.google.com/maps/embed/v1/place?key=#{@google_api_key}&q=#{sanitize(address)}"
     end
   end
 end
