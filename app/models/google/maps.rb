bff86cad27eb4813a78ab3b246208b569bb5c9f3
Kent Slaymaker <kmwalke@gmail.com>
2023-04-01 14:33:57 -0600

    redis_url in circle config

--- app/models/google/maps.rb
+++ app/models/google/maps.rb
@@ -4,14 +4,14 @@
 module Google
   class Maps < UrlParams
     GOOGLE_MAPS_BASE_URL = 'https://www.google.com/maps'.freeze
-    GOOGLE_API_KEY = ENV.fetch('GOOGLE_API_KEY').freeze
+    GOOGLE_API_KEY       = ENV.fetch('GOOGLE_API_KEY').freeze
 
     def self.query_url(address)
-      GOOGLE_MAPS_BASE_URL + '?q=' + sanitize(address)
+      "#{GOOGLE_MAPS_BASE_URL}?q=#{sanitize(address)}"
     end
 
     def self.embed_url(address)
-      "#{GOOGLE_MAPS_BASE_URL}/embed/v1/place?key=#{GOOGLE_API_KEY}" + '&q=' + sanitize(address)
+      "#{GOOGLE_MAPS_BASE_URL}/embed/v1/place?key=#{GOOGLE_API_KEY}&q=#{sanitize(address)}"
     end
   end
 end
