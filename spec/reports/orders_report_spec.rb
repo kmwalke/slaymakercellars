477bc3fabef903d7788e7080df2be600bf0c9e35
Kent Slaymaker <kmwalke@gmail.com>
2023-04-11 17:56:20 -0600

    reorg order report spec

--- spec/reports/orders_report_spec.rb
+++ spec/reports/orders_report_spec.rb
@@ -1,15 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe OrdersReport do
-  let(:order1) { create(:order, created_at: '03-01-2023') }
-  let(:order2) { create(:order, created_at: '04-04-2023') }
-  let(:order3) { create(:order, created_at: 'o4-11-2023') }
+  let(:start_date) { '04-01-2023' }
+  let(:end_date) { '04-11-2023' }
+  let(:report) { described_class.new(start_date, end_date) }
 
   describe 'initialize' do
-    let(:start_date) { '04-01-2023' }
-    let(:end_date) { '04-11-2023' }
-    let(:report) { described_class.new(start_date, end_date) }
-
     it 'sets the start date' do
       expect(report.start_date).to eq(DateTime.parse("#{start_date} 00:00:00"))
     end
@@ -21,6 +17,12 @@ RSpec.describe OrdersReport do
     it 'returns a wholesale order array' do
       expect(report.orders).to be_a(Array)
     end
+  end
+
+  describe 'report data' do
+    let!(:order1) { create(:order, created_at: '03-01-2023') }
+    let!(:order2) { create(:order, created_at: '04-04-2023') }
+    let!(:order3) { create(:order, created_at: 'o4-11-2023') }
 
     it 'returns wholesale orders' do
       expect(report.orders.empty?).to be(false)
