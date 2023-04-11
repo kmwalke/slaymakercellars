8daaaea74fe4be30195f832ea5873e2b26808f68
Kent Slaymaker <kmwalke@gmail.com>
2023-04-11 14:18:27 -0600

    build out report mock order model

--- app/reports/orders_report.rb
+++ app/reports/orders_report.rb
@@ -6,10 +6,37 @@ class OrdersReport
 
     @start_date = DateTime.parse("#{start_date} 00:00:00")
     @end_date   = DateTime.parse("#{end_date} 23:59:59")
-    @orders     = Order.where(created_at: @start_date..@end_date)
+    @orders     = wholesale_orders
   end
 
   def dates
     [@start_date, @end_date]
+  end
+
+  private
+
+  def wholesale_orders
+    Order.where(created_at: @start_date..@end_date).map do |order|
+      MockOrder.new(order)
+    end
+  end
+
+  class MockOrder
+    attr :id, :num_items, :order, :value
+
+    def initialize(order)
+      @id        = order.id
+      @order     = order
+      @num_items = count_items
+      @value     = calc_value
+    end
+
+    def count_items
+      1
+    end
+
+    def calc_value
+      15
+    end
   end
 end
