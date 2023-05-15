85cb4b64264089bb40be7e35de1766cacbc53206
Kent Slaymaker <kmwalke@gmail.com>
2023-05-15 12:07:55 -0600

    update rubocop

--- spec/reports/orders_report_spec.rb
+++ spec/reports/orders_report_spec.rb
@@ -22,7 +22,7 @@ RSpec.describe OrdersReport do
   describe 'report data' do
     let!(:order1) { create(:order, created_at: '03-01-2023') }
     let!(:order2) { create(:order, created_at: '04-04-2023') }
-    let!(:order3) { create(:order, created_at: 'o4-11-2023') }
+    let!(:order3) { create(:order, created_at: '04-11-2023') }
     let(:report2) { described_class.new(start_date, end_date) }
     let(:orders) { [order1, order2, order3] }
 
