13b1a70ddb148bed02eaeb1289abe1731f5b6ac2
Kent Slaymaker <kmwalke@gmail.com>
2023-10-22 09:55:32 -0600

    remove accidental change

--- config/application.rb
+++ config/application.rb
@@ -13,8 +13,6 @@ class App
 
     config.active_job.queue_adapter = :sidekiq
 
-    config.load_defaults 7.0
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
