8551868ce1409e46576335152a3497f6838b38e4
Kent Slaymaker <kmwalke@gmail.com>
2023-10-22 11:46:43 -0600

    add navigation specs

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -7,6 +7,18 @@ RSpec.describe 'Home' do
     expect(page).to have_content('Slaymaker Cellars')
   end
 
+  it 'story link' do
+    visit root_path
+    click_link 'Our Story'
+    expect(page).to have_current_path(root_path, ignore_query: true)
+  end
+
+  it 'contacts link' do
+    visit root_path
+    click_link 'Where to Buy'
+    expect(page).to have_current_path(contacts_path, ignore_query: true)
+  end
+
   it 'product link' do
     visit root_path
     click_link 'Meads'
@@ -17,5 +29,11 @@ RSpec.describe 'Home' do
     visit root_path
     click_link 'Visit Us'
     expect(page).to have_current_path(visit_path, ignore_query: true)
+  end
+
+  it 'menu link' do
+    visit root_path
+    click_link 'Menu'
+    expect(page).to have_current_path(menu_path, ignore_query: true)
   end
 end
