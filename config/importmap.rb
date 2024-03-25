75189e7199ab653504794de92da0705a4bebfbdd
Kent Slaymaker <kmwalke@gmail.com>
2024-03-25 12:05:57 -0600

    replace package.json with pinned importmaps

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,3 +2,7 @@
 
 pin "application"
 pin "jquery" # @3.7.1
+pin "@nathanvda/cocoon", to: "@nathanvda--cocoon.js" # @1.2.14
+pin "@rails/actioncable", to: "@rails--actioncable.js" # @7.1.3
+pin "@rails/activestorage", to: "@rails--activestorage.js" # @7.1.3
+pin "@rails/ujs", to: "@rails--ujs.js" # @7.1.3
