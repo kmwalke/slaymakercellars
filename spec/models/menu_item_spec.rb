9155d86ce15b201292dffc48a747d590fdeef683
Kent <kentslaymaker@gmail.com>
2025-03-21 13:14:37 -0600

    starting menu testing

--- spec/models/menu_item_spec.rb
+++ spec/models/menu_item_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe MenuItem, type: :model do
   it 'requires a name' do
     expect { create(:menu_item, name: '') }.to raise_error(ActiveRecord::RecordInvalid)
   end
+
+  it 'requires a sub_section' do
+    expect(described_class.create(sub_section_id: '').errors).to have_key(:sub_section_id)
+  end
 end
--- spec/models/menu_sub_section_spec.rb
+++ spec/models/menu_sub_section_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe MenuSubSection, type: :model do
   it 'requires a name' do
     expect { create(:menu_sub_section, name: '') }.to raise_error(ActiveRecord::RecordInvalid)
   end
+
+  it 'requires a section' do
+    expect(described_class.create(section_id: '').errors).to have_key(:section_id)
+  end
 end
